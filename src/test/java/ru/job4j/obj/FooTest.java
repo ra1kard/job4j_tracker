5a75f807f2228788b92174c0c4b0496257df1779
Stas Kufarev <91raykard@gmail.com>
2022-06-06 23:15:47 +0300

    задачи

---
+++ src/test/java/ru/job4j/obj/FooTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.obj;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class FooTest {
+
+    @Test
+    public void whenCreateFoo() {
+        var foo = new Foo();
+        assertThat(foo, is(notNullValue()));
+    }
+
+}
