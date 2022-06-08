e00dc2e2e53ad7384b57fc7758a92d7178acc8d3
Stas Kufarev <91raykard@gmail.com>
2022-06-08 23:10:39 +0300

    4. Переопределение [#174890] example

---
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -0,0 +1,14 @@
+package ru.job4j.inheritance;
+
+public class ReportUsage {
+
+    /**
+     * try change: TextReport report = new TextReport();
+     * */
+    public static void main(String[] args) {
+        HtmlReport report = new HtmlReport();
+        String text = report.generate("Report's name", "Report's body");
+        System.out.println(text);
+    }
+
+}
---
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+
+/**
+ * look class HtmlReport and ReportUsage
+ * */
+public class TextReport {
+
+    public String generate(String name, String body) {
+        return name + System.lineSeparator() + body;
+    }
+
+}
