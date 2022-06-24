56b80b939717744773e128f48b889cf14b977d7f
Stas Kufarev Notebook <91raykard@gmail.com>
2022-06-25 01:13:43 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -5,12 +5,45 @@ public class Tracker {
     private int ids = 1;
     private int size = 0;
 
+    /**
+     * добавление заявок:
+     * */
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
+    /**
+     * получение списка всех заявок:
+     * возвращает копию массива items без null элементов (без пустых ячеек).
+     * */
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        for (int i = 0; i < rsl.length; i++) {
+            System.out.println(rsl[i]);
+        }
+        return rsl;
+    }
+
+    /**
+     * получение списка по имени
+     * ---
+     * проверяет в цикле все элементы массива items, сравнивая name
+     * (используя метод getName класса Item) с аргументом метода String key.
+     * Элементы, у которых совпадает name, копирует в результирующий массив и
+     * возвращает его. Алгоритм этого метода аналогичен методу findAll.
+     * */
+    /*public Item[] findByName(String key) {
+
+    }*/
+
+    /**
+     * получение заявки по id
+     * ---
+     * проверяет в цикле все элементы массива items, сравнивая id с аргументом int id
+     * и возвращает найденный Item. Если Item не найден - возвращает null.
+     * */
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
@@ -22,4 +55,14 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        tracker.add(first);
+        tracker.add(second);
+        Item result = tracker.findAll()[0];
+    }
+
 }
