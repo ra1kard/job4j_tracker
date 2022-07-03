21be73ea88053464d6af36c8a1c64558052bb364
Stas Kufarev <91raykard@gmail.com>
2022-07-03 23:58:24 +0300

    ...

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,15 +1,30 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
 
+    /**
+     * инициализирует приложение и запускает выполнение различных пользовательских действий;
+     */
+    public void init(Scanner scanner, Tracker tracker) {
+
+    }
+
+    /**
+     * выводит на экран меню доступных пользовательских действий;
+     */
+    private void showMenu() {
+
+    }
+
+    /**
+     * Scanner позволит получать данные с консоли от пользователя, Tracker будет служить хранилищем заявок.
+     */
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = item.getCreated().format(formatter);
-        System.out.println(currentDateTimeFormat);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 
 }
-
