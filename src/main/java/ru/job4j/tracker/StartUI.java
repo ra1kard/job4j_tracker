b84838b0acc087eebb70f1f1bc0714acbbf45ac4
Stas Kufarev <91raykard@gmail.com>
2022-07-17 21:12:22 +0300

    2.2. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,14 +8,32 @@ public class StartUI {
      * инициализирует приложение и запускает выполнение различных пользовательских действий;
      */
     public void init(Scanner scanner, Tracker tracker) {
-
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
     }
 
     /**
      * выводит на экран меню доступных пользовательских действий;
      */
     private void showMenu() {
-
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
     }
 
     /**
