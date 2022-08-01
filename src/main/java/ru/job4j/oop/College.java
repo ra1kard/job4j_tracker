028e21ac56184ffcd7553c39e17afb64c412e357
Stas Kufarev <91raykard@gmail.com>
2022-08-01 15:35:41 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322 #327681]

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -2,7 +2,6 @@ package ru.job4j.oop;
 
 public class College {
 
-    //В нем сделайте приведение объекта Freshman к Student, а потом к Object.
     public static void main(String[] args) {
         /* создаем объекта класса Freshman. */
         Freshman freshman = new Freshman();
