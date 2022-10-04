43f76bb117cb0f29901a4175b570835f090a0f4a
Stas Kufarev <91raykard@gmail.com>
2022-10-04 13:16:48 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/JdbcConfig.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+public class JdbcConfig {
+
+    public static void load(String url) throws UserInputException {
+        if (url == null) {
+            throw new UserInputException("Url could not be null");
+        }
+    }
+
+    /** еще вариант:
+     * public static void main(String[] args) throws UserInputException {
+     *         load(jdbc://localhost:8080);
+     * }
+     */
+    public static void main(String[] args) {
+        try {
+            load("jdbc://localhost:8080");
+        } catch (UserInputException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/ru/job4j/ex/UserInputException.java
@@ -0,0 +1,9 @@
+package ru.job4j.ex;
+
+public class UserInputException extends Exception {
+
+    public UserInputException(String message) {
+        super(message);
+    }
+
+}
