b52bdaddf2b0174014e0cbfe6bc226ccc9be3a36
Stas Kufarev <91raykard@gmail.com>
2022-10-14 18:57:13 +0300

    2.1. Тест на ValidateInput [#311887 #350353]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ValidateInputTest {
 
+    /**
+     * Тест whenInvalidInput проверяет ситуацию, когда пользователь ввел сначала неверные данные, а потом верные.
+     */
     @Test
     public void whenInvalidInput() {
         Output out = new StubOutput();
@@ -12,30 +15,45 @@ public class ValidateInputTest {
                 new String[] {"one", "1"}
         );
         ValidateInput input = new ValidateInput(out, in);
-        int selected = input.askInt("Enter menu:");
+        int selected = input.askInt("Menu:");
         assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenMultipleValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "2", "3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Menu:");
+        int selectedTwo = input.askInt("Menu:");
+        int selectedThree = input.askInt("Menu:");
+        assertThat(selected).isEqualTo(1);
+        assertThat(selectedTwo).isEqualTo(2);
+        assertThat(selectedThree).isEqualTo(3);
     }
 
     @Test
     public void whenValidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0"}
+                new String[] {"1"}
         );
         ValidateInput input = new ValidateInput(out, in);
-        String selected = input.askStr("=== Create a new Item ===");
-        assertThat(selected).isEqualTo("0");
+        int selected = input.askInt("Menu:");
+        assertThat(selected).isEqualTo(1);
     }
 
     @Test
-    public void whenInvalidInputNegativeNumber() {
+    public void whenValidInputNegativeNumber() {
         Output out = new StubOutput();
         Input in = new StubInput(
                 new String[] {"-2"}
         );
         ValidateInput input = new ValidateInput(out, in);
-        String selected = input.askStr("Wrong input, you can select: 0 .. 6");
-        assertThat(selected).isEqualTo("-2");
+        int selected = input.askInt("Menu:");
+        assertThat(selected).isEqualTo(-2);
     }
 
 }
