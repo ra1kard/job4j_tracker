3e17d265ae0ef4e59e19ee5190f11be35cf3892d
Stas Kufarev <91raykard@gmail.com>
2022-10-18 15:42:23 +0300

    1. Singleton [#94606]_v3

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,25 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+    private Tracker tracker = new Tracker();
+    private static SingleTracker instance = null;
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return null;
+    }
+
+}
